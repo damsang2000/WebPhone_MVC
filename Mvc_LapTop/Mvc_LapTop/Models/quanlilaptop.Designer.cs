329a91769490df5c094ec300068305cc9a7615a6
damsang <damsang163@gmail.com>
2021-06-28 15:03:45 +0700

    web_mvc

--- Mvc_LapTop/Mvc_LapTop/Models/quanlilaptop.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\ASUS\Desktop\BÁO CÁO WEB\Mvc_LapTop\Mvc_LapTop\Models\quanlilaptop.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
